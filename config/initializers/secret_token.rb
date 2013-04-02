3294e52002fbd0537cb49e212ee82e5fb6a23300
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-04-02 13:41:17 -0400

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoraSalud::Application.config.secret_token = '7e46508605773dffdad207396a6b849c9f92c6f204e805a4583efd3214e067096128be5f7204109e1115b082f92b73cd102c05246c2af0139d4e9986fcdf7860'
