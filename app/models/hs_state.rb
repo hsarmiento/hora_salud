9b42313164db8a1dba7c263a962c634410873bd1
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-04-04 14:01:46 -0400

    Se agrega logo en home y dependencia entre region-comuna

--- app/models/hs_state.rb
+++ app/models/hs_state.rb
@@ -1,3 +1,4 @@
 class HsState < ActiveRecord::Base
   attr_accessible :name, :order
+  has_many :hs_townships
 end
--- app/models/hs_township.rb
+++ app/models/hs_township.rb
@@ -1,3 +1,4 @@
 class HsTownship < ActiveRecord::Base
   attr_accessible :hs_state_id, :name
+  belongs_to :hs_state
 end
