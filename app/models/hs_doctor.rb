302c01863b7a51cb606e5b16962339ce1b773448
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-04-05 15:51:26 -0400

    Se corrige buscador de doctores para buscar por nombre y apellido

--- app/models/hs_doctor.rb
+++ app/models/hs_doctor.rb
@@ -6,9 +6,9 @@ class HsDoctor < ActiveRecord::Base
   def self.search(like,state,township)
   	like_condition = "%" + like + "%"
   	if township!=""
-  		joins(:hs_clinic).where('(hs_doctors.names ILIKE ? OR hs_doctors.specialty ILIKE ? OR hs_doctors.last_names ILIKE ?) AND (hs_clinics.hs_state_id = ? AND hs_clinics.township = ?)', like_condition, like_condition, like_condition, state, township)
+  		joins(:hs_clinic).where("(hs_doctors.names ILIKE ? OR hs_doctors.specialty ILIKE ? OR hs_doctors.last_names ILIKE ? OR hs_doctors.names ||' '|| hs_doctors.last_names ILIKE ?) AND (hs_clinics.hs_state_id = ? AND hs_clinics.township = ?)", like_condition, like_condition, like_condition, like_condition, state, township)
   	else
-		joins(:hs_clinic).where('(hs_doctors.names ILIKE ? OR hs_doctors.specialty ILIKE ? OR hs_doctors.last_names ILIKE ?) AND (hs_clinics.hs_state_id = ?)', like_condition, like_condition, like_condition, state)
+		  joins(:hs_clinic).where("(hs_doctors.names ILIKE ? OR hs_doctors.specialty ILIKE ? OR hs_doctors.last_names ILIKE ? OR hs_doctors.names ||' '|| hs_doctors.last_names ILIKE ?) AND (hs_clinics.hs_state_id = ?)", like_condition, like_condition, like_condition, like_condition, state)
   	end  
   	#find(:all, conditions: ['name LIKE ? OR specialty LIKE ? OR last_name LIKE ?', search_condition, search_condition, search_condition])
   end
