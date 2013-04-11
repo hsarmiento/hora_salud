ba98f01a2e80f9b8430e14d28dab35c9534328cf
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-04-11 18:02:24 -0400

    Se agrega envio de correo por form de contacto

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,18 @@ class StaticPagesController < ApplicationController
 		@states = HsState.all
 		@townships = HsTownship.all
 	end
+
+	def contact
+		
+	end
+
+	def enviar
+		email = params[:email]
+		nombre = params[:nombre]
+		telefono = params[:telefono]
+		comentario = params[:comentario]
+		body = "nombre:#{nombre}\r\n email:#{email}\r\n telefono:#{telefono} \r\n comentario:#{comentario}"
+		ContactMailer.contact_us(email,nombre,body).deliver
+		redirect_to contacto_path
+	end
 end
